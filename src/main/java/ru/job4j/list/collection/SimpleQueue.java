4afe6d6de4a417a9e5037b61020d2a26a273cfe8
IlyaKuligin <vbG764Gjskdi>
2021-11-28 19:47:57 +0300

    5. Очередь на двух стеках (Исправления).

--- src/main/java/ru/job4j/list/collection/SimpleQueue.java
+++ src/main/java/ru/job4j/list/collection/SimpleQueue.java
@@ -4,25 +4,20 @@ public class SimpleQueue<T> {
 
     private final SimpleStack<T> in = new SimpleStack<>();
     private final SimpleStack<T> out = new SimpleStack<>();
-    private int count = 0;
 
     public T poll() {
-        int count = this.count;
         T rsl;
-        while (this.count != 0) {
+        while (in.hasNext()) {
             out.push(in.pop());
-            this.count--;
         }
         rsl = out.pop();
-        while (this.count != count - 1) {
+        while (out.hasNext()) {
             in.push(out.pop());
-            this.count++;
         }
         return rsl;
     }
 
     public void push(T value) {
         in.push(value);
-        count++;
     }
 }
