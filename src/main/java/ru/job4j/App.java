e04a28c7876e509d5ea8322b52dc5c941e99e568
IlyaKuligin <vbG764Gjskdi>
2021-12-02 18:18:19 +0300

    2. Без переопределения equals и hashCode.

--- src/main/java/ru/job4j/App.java
+++ src/main/java/ru/job4j/App.java
@@ -1,12 +1,21 @@
 package ru.job4j;
 
-/**
- * Hello world!
- *
- */
+import ru.job4j.map.User;
+
+import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Map;
+
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        User ingenier = new User("Ivan", 2,
+                new GregorianCalendar(1986, 2, 25));
+        User programmer = new User("Ivan", 2,
+                new GregorianCalendar(1986, 2, 25));
+        Map<User, Object> map = new HashMap<>();
+        map.put(ingenier, new Object());
+        map.put(programmer, new Object());
+        System.out.println(map);
     }
 }
