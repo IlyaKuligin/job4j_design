4ad4f20e20326065488d5c3455fb398990254146
IlyaKuligin <vbG764Gjskdi>
2021-12-21 11:16:11 +0300

    5. Валидация параметров запуска.

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -1,5 +1,6 @@
 package ru.job4j.io;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -10,12 +11,22 @@ import java.util.function.Predicate;
 public class Search {
 
     public static void main(String[] args) throws IOException {
+        search(validateParameters(args), p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+    }
+
+    public static Path validateParameters(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException(
                     "Root folder or file format is null. Usage java -jar dir.jar ROOT_FOLDER FILE_FORMAT.");
         }
         Path start = Paths.get(args[0]);
-        search(start, p -> p.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        if (!Files.exists(start)) {
+            throw new IllegalArgumentException(String.format("Not exist %s", start.toAbsolutePath()));
+        }
+        if (!Files.isDirectory(start)) {
+            throw new IllegalArgumentException(String.format("Not directory %s", start.toAbsolutePath()));
+        }
+        return start;
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
