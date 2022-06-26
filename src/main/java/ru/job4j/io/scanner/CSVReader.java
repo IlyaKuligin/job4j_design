bd5bb68738cf51b745df439b947822cec3705180
IlyaKuligin <ilya.kuligin.russia@gmail.com>
2022-06-26 15:23:16 +0300

    7. Scanner (исправления) [#504791]

--- src/main/java/ru/job4j/io/scanner/CSVReader.java
+++ src/main/java/ru/job4j/io/scanner/CSVReader.java
@@ -6,13 +6,14 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class CSVReader {
 
     public static void handle(ArgsName argsName) throws Exception {
-        try (BufferedReader in = new BufferedReader(new FileReader(argsName.get("path"), StandardCharsets.UTF_8));
-             PrintWriter out = new PrintWriter(new BufferedOutputStream(new FileOutputStream(argsName.get("out"))))) {
+        try (BufferedReader in = new BufferedReader(new FileReader(argsName.get("path"), StandardCharsets.UTF_8))) {
+            StringBuilder out = new StringBuilder();
             try (var scanner = new Scanner(in).useDelimiter(argsName.get("delimiter"))) {
                 String[] filtersElements = argsName.get("filter").split(",");
                 String[] nameElements = scanner.nextLine().split(";");
@@ -21,25 +22,33 @@ public class CSVReader {
                     for (String el : filtersElements) {
                         if (el.equals(nameElements[i])) {
                             if (i == 0) {
-                                out.print(nameElements[i]);
+                                out.append(nameElements[i]);
                             } else {
-                                out.print(";" + nameElements[i]);
+                                out.append(";" + nameElements[i]);
                             }
                             indexes.add(i);
                         }
                     }
                 }
-                out.print(System.lineSeparator());
+                out.append(System.lineSeparator());
                 while (scanner.hasNextLine()) {
                     nameElements = scanner.nextLine().split(";");
                     for (Integer index : indexes) {
-                        if (index == indexes.get(0)) {
-                            out.print(nameElements[index]);
+                        if (Objects.equals(index, indexes.get(0))) {
+                            out.append(nameElements[index]);
                         } else {
-                            out.print(";" + nameElements[index]);
+                            out.append(";" + nameElements[index]);
                         }
                     }
-                    out.print(System.lineSeparator());
+                    out.append(System.lineSeparator());
+                }
+            }
+            if (argsName.get("out").equals("stdout")) {
+                System.out.print(out.toString());
+            } else {
+                try (PrintWriter outfile =
+                             new PrintWriter(new BufferedOutputStream(new FileOutputStream(argsName.get("out"))))) {
+                    outfile.print(out);
                 }
             }
         } catch (IOException e) {
@@ -48,12 +57,20 @@ public class CSVReader {
     }
 
     private static boolean validateParameters(String[] args) {
-        boolean rsl = false;
         if (args.length != 4) {
             throw new IllegalArgumentException("Wrong number of arguments");
         }
-        rsl = true;
-        return rsl;
+        for (String s : args) {
+            if (!s.startsWith("-") || !s.contains("=") || (s.indexOf('=') != s.lastIndexOf('='))) {
+                throw new IllegalArgumentException("Некорректно введены аргументы");
+            }
+            s = s.substring(1);
+            String[] parametrs = s.split("=");
+            if (parametrs.length != 2) {
+                throw new IllegalArgumentException("Некорректно введены аргументы");
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) throws Exception {
