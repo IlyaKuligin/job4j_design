bf04573d81ad1c6a722d0307eb8118bfca30fc58
IlyaKuligin <ilya.kuligin.russia@gmail.com>
2022-07-30 09:22:06 +0300

    0. Select [#6861]

---
+++ src/sql/insert_fauna.sql
@@ -0,0 +1,6 @@
+insert into fauna(name, avg_age, discovery_date)
+values ('Белый медведь', 10950, date '1774-01-01');
+insert into fauna(name, avg_age, discovery_date)
+values ('Обыкновенный ёж', 2190, null);
+insert into fauna(name, avg_age, discovery_date)
+values ('Lasiognathus dinema fish', null, date '2015-01-01');
